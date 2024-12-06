d80d96d07fe293e06a4c898b4973c55f6ea6a4fb
Ewrdad <Ewrdad12+git@gmail.com>
2024-12-06 18:41:22 +0000

    a rough template

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,29 @@
+
+
+export const Home = () => {
+
+
+    return (
+        <div className="self-start w-screen h-[90vh] justify-between ">
+
+            <div className="h-1/3">
+
+            <h1 className="text-[30vh] align-middle hover:text-slate-700">Code</h1>
+
+            </div>
+
+            <div className="h-1/3">
+
+            <h1 className="text-[30vh] align-middle hover:text-slate-700">Change</h1>
+
+            </div>
+
+            <div className="h-1/3">
+
+            <h1 className="text-[30vh] align-middle hover:text-slate-700">Impact</h1>
+
+            </div>
+
+        </div>
+    )
+}
---
+++ src/sidebar/sidebar.jsx
@@ -0,0 +1,106 @@
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarHeader,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarGroupLabel,
+    SidebarGroupContent,
+    SidebarMenu
+    
+  } from "@/components/ui/sidebar"
+  
+  export function AppSidebar() {
+    return (
+      <Sidebar >
+        <SidebarHeader >
+<SidebarMenu>
+        <SidebarMenuItem>
+              <SidebarMenuButton>
+                Edward Jones
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+            </SidebarMenu>
+        </SidebarHeader>
+
+        <SidebarContent >
+
+          <SidebarGroupLabel>Work</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Raytheon</SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>UKAEA</SidebarMenuButton>
+            </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+         
+
+          <SidebarGroupLabel>Certifcations</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>AWS</SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>CompTIA</SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Azure</SidebarMenuButton>
+            </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+
+          <SidebarGroupLabel>Proffesional Development</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>BCS Membership</SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Online Training</SidebarMenuButton>
+            </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+
+
+          <SidebarGroupLabel>Projects</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Portfolio</SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>JsUseDocs</SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>LiveQuery</SidebarMenuButton>
+            </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+
+          <SidebarGroupLabel>External Activities</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Enactus</SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Cardiff Youth Council</SidebarMenuButton>
+            </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+
+        </SidebarContent>
+
+        
+
+        <SidebarFooter />
+      </Sidebar>
+    )
+  }
+  
