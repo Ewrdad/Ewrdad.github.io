e9c07133ebf605466f029ff81b0b45591e278e73
Ewrdad <Ewrdad12+git@gmail.com>
2024-12-06 16:38:59 +0000

    added in chadcn ui

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build:{
     outDir: 'docs'
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(path.dirname(__filename), "./src"),
+
+    },
+  },
 })
